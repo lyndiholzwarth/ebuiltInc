30b2120009d5c75c0dda730153a8d32178092a1c
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-07-15 13:43:31 -0400

    company apge complete

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 $(document) .ready(function() {
 
-
-
-
+      google.maps.event.addDomListener(window, 'load', drawMap);
  
 });
+
+
+function drawMap() {
+	var myLatlng = new google.maps.LatLng(39.971274,-75.130922);
+	var mapOptions = {
+      center: myLatlng,
+      zoom: 16,
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'eBuilt Inc'
+  });
+};
